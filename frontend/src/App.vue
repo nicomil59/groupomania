bb05fc4ff727347f2d0288dcc42b0e6c3cfefb8e
Nicolas Milhem <nicolasmilhem@gmail.com>
2022-04-11 16:09:28 +0200

    mise en place Frontend vue 3 et bootstrap

---
+++ frontend/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="vue-app">
+    <NavBar />
+    <h1>Test</h1>
+  </div>
+</template>
+
+<script>
+import NavBar from "@/components/NavBar.vue";
+
+export default {
+  name: "App",
+  components: {
+    NavBar,
+  },
+};
+</script>
+
+<style>
+body {
+  background-color: #fff;
+}
+</style>
---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,47 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+      <a class="navbar-brand" href="#">
+        <img
+          src="../assets/icon-left-font.png"
+          alt="logo Groupomania"
+          class="d-inline-block align-text-top"
+        />
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Se connecter</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">S'inscrire</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+};
+</script>
+
+<style scoped>
+img {
+  height: 50px;
+}
+
+</style>
